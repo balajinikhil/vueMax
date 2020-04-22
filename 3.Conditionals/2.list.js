eb15f1754daa1fc99d3f9fec0dd12e8602b345dc
balajinikhil <balajiedward007@gmail.com>
2020-04-23 03:04:44 +0530

    conditionals

---
+++ 3.Conditionals/2.list.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: "#app",
+  data: {
+    fruits: ["apple", "mango", "orange", "grapes"],
+    person: [
+      {
+        name: "Max",
+        age: "21",
+        team: "redbull",
+      },
+      {
+        name: "Seb",
+        age: "33",
+        team: "ferrari",
+      },
+    ],
+  },
+});
